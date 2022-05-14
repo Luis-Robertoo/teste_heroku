220d13731ae892d77e0543579ad1e012c9d51f1a
Luis Roberto <luis.r.s14@hotmail.com>
2022-05-14 14:30:27 -0300

    corrigido config

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-
 const server = express()
+const PORT = process.env.PORT || 5000
 
 server.get('/', (req, res) => {
     res.send('olá mundo, começo das coisas')
 })
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
     console.log('iniciei')
 })
