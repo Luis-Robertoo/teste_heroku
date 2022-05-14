781fbae81ef2373aeefd6315ef044254a738c6ea
Luis Roberto <luis.r.s14@hotmail.com>
2022-05-14 14:42:56 -0300

    testando variaveis globais

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ server.get('/', (req, res) => {
     res.send('olá mundo, começo das coisas')
 })
 
+server.get('/tempo', (req, res) => {
+    res.send(mostraVar())
+})
+
 server.listen(PORT, () => {
     console.log('iniciei')
 })
+
+
+
+mostraVar = () => {
+    let resultado = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+        resultado += i + ' ';
+    }
+    return resultado;
+}
