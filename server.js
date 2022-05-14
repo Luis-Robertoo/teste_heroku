4af7c65169d5574337638cd809fc8b0461c8bf97
Luis Roberto <luis.r.s14@hotmail.com>
2022-05-14 15:19:37 -0300

    adicionado o banco

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 server.get('/', (req, res) => {
     res.send('olá mundo, começo das coisas')
 })
 
-server.get('/tempo', (req, res) => {
-    res.send(mostraVar())
+server.get('/tempo', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM teste');
+      const results = { 'results': (result) ? result.rows : null};
+      
+      res.send("resultados " + results);
+    }catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+  })
+
+server.get('/db', (req, res) => {
+
 })
 
 server.listen(PORT, () => {
